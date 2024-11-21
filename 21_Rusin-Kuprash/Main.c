13dcaec54f3fb3ef20965bc85594666b83b5ae4a
Григорий Русин-Купраш <grigoriirusinkuprash@gmail.com>
2024-11-21 10:17:07 +0300

    BubbleSort heap corruption fixed

--- 21_Rusin-Kuprash/Main.c
+++ 21_Rusin-Kuprash/Main.c
@@ -33,6 +33,14 @@ void swap(float *a, float *b, int *h) {
 }
 
 
+int checkListSorted(float list[], int n) {
+	int ch = 1;
+	int i = 0;
+	while ((i < n - 1) || ch == 0) ch = list[i] > list[i++ + 1];
+	return ch;
+}
+
+
 void DoubleBubble(float* const list, int n, int* checks, int* swaps) {
 	int l = 0, r = 0;
 	int dlt = 0;
@@ -40,7 +48,7 @@ void DoubleBubble(float* const list, int n, int* checks, int* swaps) {
 		//Тянем вправо.
 		if (i % 2) {
 			dlt = 1;
-			for (int j = l; j + r < n; j++) {
+			for (int j = l; j + r + 1 < n; j++) {
 				if (check(list[j], list[j + 1], checks)) {
 					swap(&list[j], &list[j + 1], swaps);
 					dlt = 1;
@@ -52,7 +60,7 @@ void DoubleBubble(float* const list, int n, int* checks, int* swaps) {
 		//Тянем влево.
 		else {
 			dlt = 1;
-			for (int j = n - r - 1; j - 1 > l; j--) {
+			for (int j = n - r - 1; j> l; j--) {
 				if (check(list[j - 1], list[j], checks)) {
 					swap(&list[j - 1], &list[j], swaps);
 					dlt = 1;
@@ -80,24 +88,27 @@ void listPrint(float list[1000], int n) {
 
 
 //state = 0: База - изменение массива
-void showBaseScreen(float list[1000], int n) {
+void showBaseScreen(float list[1000], int *n) {
 	int input;
-	listPrint(list, n);
+	float cop[9] = {5, 4, 1, 2, 5, 6, 9, 7, 8};
+	listPrint(list, *n);
 
 	//Панель выбора.
 	printf("\n1) Заполнить массив (только при n < 11).\n");
 	printf("2) Случайно заполнить массив.\n");
 	printf("3) Изменить размер массива.\n");
-	printf("\n4) Отсортировать массив.\n");
+	printf("4) Выбрать заготовленный массив.\n");
+	printf("\n5) Отсортировать массив.\n");
 	printf("\n25) Выйти.\n");
 
 	input = 0;
 	scanf_s("%d", &input);
 	switch (input) {
 	case(1): state = 1; break;
-	case(2): fillRandList(list, n); printf("Массив случайно сгенерирован;)\n"); break;
+	case(2): fillRandList(list, *n); printf("Массив случайно сгенерирован;)\n"); break;
 	case(3): state = 2; break;
-	case(4): state = 3; break;
+	case(4): memcpy(list, cop, (sizeof(float) * 9)); *n = 9; break;
+	case(5): state = 3; break;
 	case(25): state = 255; break;
 	}
 }
@@ -133,36 +144,39 @@ void listSizeChangeScreen(float list[1000], int *n) {
 }
 
 //state = 3: Сортировки
-void sortScreen(float list[1000], int n, LARGE_INTEGER freq) {
+void sortScreen(float list[], int n, LARGE_INTEGER freq) {
 	int input;
 	LARGE_INTEGER start, finish;
 	int checks = 0, swaps = 0;
-	float* const sr_list = (float*)malloc(sizeof(float) * n);
+	float* sr_list = (float*)malloc(sizeof(float) * n);
 	state = 0;
 	
-	if (sr_list == 0) printf("Нет памяти((.");
+	if (sr_list == NULL) printf("Нет памяти((.\n\n");
 	else {
 		listPrint(list, n);
 
 		memcpy(sr_list, list, sizeof(float) * n);
 
 		printf("\n1) Двунаправленный пузывёк.\n");
-		printf("2) Случайно заполнить массив.\n");
-		printf("3) Изменить размер массива.\n");
-		printf("\n4) Отсортировать массив.\n");
+		//printf("2) Случайно заполнить массив.\n");
+		//printf("3) Изменить размер массива.\n");
+		//printf("\n4) Отсортировать массив.\n");
 		scanf_s("%d", &input);
 
 		
-
+		QueryPerformanceCounter(&start);
 		switch (input) {
 		case(1): 
-			QueryPerformanceCounter(&start);
 			DoubleBubble(sr_list, n, &checks, &swaps);
-			QueryPerformanceCounter(&finish);
-			printf("Время выполнения: %lf", (double)(finish.QuadPart - start.QuadPart) / (double)freq.QuadPart);
-
 			break;
 		}
+		QueryPerformanceCounter(&finish);
+		printf("%d\n\n", checkListSorted(sr_list, n));
+
+		listPrint(sr_list, n);
+		printf("\nВремя выполнения: %lf\n", (double)(finish.QuadPart - start.QuadPart) / (double)freq.QuadPart);
+		printf("Кол-во проверок: %d, Кол-во перемен: %d\n\n", checks, swaps);
+		free(sr_list);
 	}
 	
 }
@@ -186,10 +200,10 @@ int main() {
 			state = 0;
 		}
 		switch (state) {
-		case(0): showBaseScreen(list, n); break;
+		case(0): showBaseScreen(list, &n); break;
 		case(1): manualFillScreen(list, n); break;
 		case(2): listSizeChangeScreen(list, &n); break;
-		case(3): listSizeChangeScreen(list, &n); break;
+		case(3): sortScreen(list, n, freq); break;
 		}
 	}
 }
