856b1d8a4d138bc44b11469cbd5f4d97de57015e
Timofey Petrov <ptim0123456789@gmail.com>
2024-11-24 15:14:35 +0300

    ДЗ Петров Тимофей Двумерный Массивы
    ДЗ Петров Тимофей Двумерный Массивы

---
+++ DZ.c
@@ -0,0 +1,82 @@
+﻿//1 Задача на массивы средняя сложность номер 19
+#include <stdio.h>
+
+void count_marks(int a[][3], int n)
+{
+	int i, j, ocenka;
+	int neyvka = 0, neyd = 0, ydovl = 0, hor = 0, otl = 0;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			ocenka = a[i][j];
+			switch (ocenka)
+			{
+			case 1:
+				neyvka++;
+				break;
+			case 2:
+				neyd++;
+				break;
+			case 3:
+				ydovl++;
+				break;
+			case 4:
+				hor++;
+				break;
+			case 5:
+				otl++;
+				break;
+			default:
+				printf("Incorrect ocenka\n");
+				printf("Will be replaces as 1\n");
+				ocenka = 1;
+				break;
+			}
+		}
+	}
+	printf("neyvka = %d\n", neyvka);
+	printf("neyd = %d\n", neyd);
+	printf("ydovl = %d\n", ydovl);
+	printf("hor = %d\n", hor);
+	printf("otl = %d\n", otl);
+}
+
+void check_matrix(int a[][3], int n)
+{
+	int i, j;
+	for (i = 0; i < n; i++)
+	{
+		printf("student %d: ", i + 1);
+		for (j = 0; j < 3; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int marks[1000][3];
+	int n;
+	int ocenka;
+	int i, j;
+	printf("Max students count is 1000\n");
+	printf("Enter students count: \n");
+	scanf_s("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			printf("Enter students ocenka: \n");
+			scanf_s("%d", &ocenka);
+			marks[i][j] = ocenka;
+		}
+		printf("------------------\n");
+	}
+	check_matrix(marks, n);
+	printf("------------------\n");
+	count_marks(marks, n);
+	return 0;
+}
---
+++ DZ2.c
@@ -0,0 +1,68 @@
+//2 ?????? ?? ??????? ????? 13
+#include <stdio.h>
+
+int matrix[1000][1000];
+int matrix_result[1000][1000];
+
+void check_matrix(int a[][1000], int n, int m)
+{
+	int i, j;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			printf("%d ", a[i][j]);
+		}	
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int m, n;
+	int minimum = 1000000, stroks_minimum = -1;
+	int i, j;
+	int matrix_element = 0;
+	int flag = 0;
+	printf("Len stroka (m <= 999): ");
+	scanf_s("%d", &m);
+	printf("Len stolbec (n <= 999): ");
+	scanf_s("%d", &n);
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			scanf_s("%d", &matrix_element);
+			matrix[i][j] = matrix_element;
+			if (matrix_element < minimum)
+			{
+				minimum = matrix_element;
+				stroks_minimum = i;
+			}
+		}
+	}
+	printf("------------------\n");
+	check_matrix(matrix, n, m);
+	for (i = 0; i < m; i++)
+	{
+		if (i == stroks_minimum)
+			flag = 1;
+		if (flag == 0)
+		{
+			for (j = 0; j < n; j++)
+			{
+				matrix_result[i][j] = matrix[i][j];
+			}
+		}
+		else
+		{
+			for (j = 0; j < n; j++)
+			{
+				matrix_result[i][j] = matrix[i + 1][j];
+			}
+		}
+	}
+	printf("------------------\n");
+	check_matrix(matrix_result, n - 1, m);
+	return 0;
+}
