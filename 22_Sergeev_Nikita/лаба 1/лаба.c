c9ebf5320517b5b2279f217e7f8d483e97e58584
Nikita Serqeev <serqeev3@gmail.com>
2024-12-02 13:37:39 +0300

    лаба добавил быструю сортировку и сортировку слиянием

--- 22_Sergeev_Nikita/лаба 1/лаба.c
+++ 22_Sergeev_Nikita/лаба 1/лаба.c
@@ -1,9 +1,8 @@
 #include <locale.h>
 #include <stdio.h>
-#include <math.h>
-#include <time.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 void menu() {
     printf("|1.Рандомный массив       |\n");
@@ -14,8 +13,10 @@ void menu() {
     printf("|5.сортировка выбором     |\n");
     printf("|6.сортировка подсчетом   |\n");
     printf("|7.сортировка шелла       |\n");
+    printf("|8.быстрая сортировка     |\n");
+    printf("|9.сортировка слиянием    |\n");
     printf("---------------------------\n");
-    printf("|8.выход                  |\n\n");
+    printf("|10.выход                 |\n\n");
     printf("выбор: ");
 }
 
@@ -53,7 +54,6 @@ void selectionSort(int arr[], int n, int* iterationCount) {
 
 void countingSort(int arr[], int size, int* iterationCount) {
     *iterationCount = 0;
-
     int min = arr[0];
     int max = arr[0];
     for (int i = 1; i < size; i++) {
@@ -67,9 +67,7 @@ void countingSort(int arr[], int size, int* iterationCount) {
     }
 
     int range = max - min + 1;
-
     int* count = (int*)calloc(range, sizeof(int));
-
 
     for (int i = 0; i < size; i++) {
         count[arr[i] - min]++;
@@ -104,6 +102,84 @@ void shellSort(int arr[], int size, int* iterationCount) {
     }
 }
 
+void quickSort(int arr[], int low, int high, int* iterationCount) {
+    if (low < high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            (*iterationCount)++;
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        
+        quickSort(arr, low, i, iterationCount);
+        quickSort(arr, i + 2, high, iterationCount);
+    }
+}
+
+void merge(int arr[], int left, int right, int* iterationCount) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        merge(arr, left, mid, iterationCount);
+        merge(arr, mid + 1, right, iterationCount);
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int* L = (int*)malloc(n1 * sizeof(int));
+        int* R = (int*)malloc(n2 * sizeof(int));
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[left + i];
+            (*iterationCount)++;
+        }
+        for (int i = 0; i < n2; i++) {
+            R[i] = arr[mid + 1 + i];
+            (*iterationCount)++;
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            (*iterationCount)++;
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            }
+            else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        while (i < n1) {
+            arr[k++] = L[i++];
+            (*iterationCount)++;
+        }
+        while (j < n2) {
+            arr[k++] = R[j++];
+            (*iterationCount)++;
+        }
+
+        free(L);
+        free(R);
+    }
+}
+
+void mergeSort(int arr[], int left, int right, int* iterationCount) {
+    merge(arr, left, right, iterationCount);
+}
+
 int isSorted(int arr[], int size) {
     for (int i = 0; i < size - 1; i++) {
         if (arr[i] > arr[i + 1]) {
@@ -113,21 +189,19 @@ int isSorted(int arr[], int size) {
     return 1;
 }
 
-int* globalArray = NULL;
-int* originalArray = NULL;
-int globalArraySize = 0;
-
-void restoreArray() {
-    if (globalArray != NULL && originalArray != NULL) {
-        memcpy(globalArray, originalArray, globalArraySize * sizeof(int));
-    }
+void restoreArray(int* arr, int* originalArray, int size) {
+    memcpy(arr, originalArray, size * sizeof(int));
 }
 
 int main() {
     int vib = 0;
     setlocale(LC_ALL, "rus");
+    int globalArraySize = 0;
+    int* globalArray = NULL;
+    int* originalArray = NULL;
+
     menu();
-    while (vib != 8) {
+    while (vib != 10) {
         scanf_s("%d", &vib);
         switch (vib) {
         case 1: {
@@ -187,17 +261,9 @@ int main() {
                 clock_t end = clock();
                 double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
 
-                printf("\nОтсортированный массив (сортировка вставками)");
-
-                printf("\nКоличество итераций: %d\n", iterations);
-                printf("Время выполнения: %.6f секунд\n\n", time_spent);
-                if (isSorted(globalArray, globalArraySize) == 1) {
-                    printf("Массив успешно отсортирован.\n\n");
-                }
-                else {
-                    printf("Ошибка: массив не отсортирован.\n\n");
-                }
-                restoreArray();
+                printf("\nОтсортированный массив (сортировка вставками)\n");
+                printf("Количество итераций: %d\n", iterations);
+                printf("Время сортировки: %f секунд\n", time_spent);
             }
             else {
                 printf("Массив не инициализирован. Сначала введите или создайте массив.\n\n");
@@ -213,17 +279,9 @@ int main() {
                 clock_t end = clock();
                 double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
 
-                printf("\nОтсортированный массив (сортировка выбором)");
-
-                printf("\nКоличество итераций: %d\n", iterations);
-                printf("Время выполнения: %.6f секунд\n\n", time_spent);
-                if (isSorted(globalArray, globalArraySize) == 1) {
-                    printf("Массив успешно отсортирован.\n\n");
-                }
-                else {
-                    printf("Ошибка: массив не отсортирован.\n\n");
-                }
-                restoreArray();
+                printf("\nОтсортированный массив (сортировка выбором)\n");
+                printf("Количество итераций: %d\n", iterations);
+                printf("Время сортировки: %f секунд\n", time_spent);
             }
             else {
                 printf("Массив не инициализирован. Сначала введите или создайте массив.\n\n");
@@ -239,17 +297,9 @@ int main() {
                 clock_t end = clock();
                 double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
 
-                printf("\nОтсортированный массив (сортировка подсчетом)");
-
-                printf("\nКоличество итераций: %d\n", iterations);
-                printf("Время выполнения: %.6f секунд\n\n", time_spent);
-                if (isSorted(globalArray, globalArraySize) == 1) {
-                    printf("Массив успешно отсортирован.\n\n");
-                }
-                else {
-                    printf("Ошибка: массив не отсортирован.\n\n");
-                }
-                restoreArray();
+                printf("\nОтсортированный массив (сортировка подсчетом)\n");
+                printf("Количество итераций: %d\n", iterations);
+                printf("Время сортировки: %f секунд\n", time_spent);
             }
             else {
                 printf("Массив не инициализирован. Сначала введите или создайте массив.\n\n");
@@ -265,17 +315,9 @@ int main() {
                 clock_t end = clock();
                 double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
 
-                printf("\nОтсортированный массив (сортировка шелла)");
-
-                printf("\nКоличество итераций: %d\n", iterations);
-                printf("Время выполнения: %.6f секунд\n\n", time_spent);
-                if (isSorted(globalArray, globalArraySize) == 1) {
-                    printf("Массив успешно отсортирован.\n\n");
-                }
-                else {
-                    printf("Ошибка: массив не отсортирован.\n\n");
-                }
-                restoreArray();
+                printf("\nОтсортированный массив (сортировка Шелла)\n");
+                printf("Количество итераций: %d\n", iterations);
+                printf("Время сортировки: %f секунд\n", time_spent);
             }
             else {
                 printf("Массив не инициализирован. Сначала введите или создайте массив.\n\n");
@@ -283,12 +325,57 @@ int main() {
             menu();
             break;
         }
+        case 8: {
+            if (globalArray != NULL && globalArraySize > 0) {
+                int iterations = 0;
+                clock_t start = clock();
+                quickSort(globalArray, 0, globalArraySize - 1, &iterations);
+                clock_t end = clock();
+                double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+
+                printf("\nОтсортированный массив (быстрая сортировка)\n");
+                printf("Количество итераций: %d\n", iterations);
+                printf("Время сортировки: %f секунд\n", time_spent);
+            }
+            else {
+                printf("Массив не инициализирован. Сначала введите или создайте массив.\n\n");
+            }
+            menu();
+            break;
+        }
+        case 9: {
+            if (globalArray != NULL && globalArraySize > 0) {
+                int iterations = 0;
+                clock_t start = clock();
+                mergeSort(globalArray, 0, globalArraySize - 1, &iterations);
+                clock_t end = clock();
+                double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+
+                printf("\nОтсортированный массив (сортировка слиянием)\n");
+                printf("Количество итераций: %d\n", iterations);
+                printf("Время сортировки: %f секунд\n", time_spent);
+            }
+            else {
+                printf("Массив не инициализирован. Сначала введите или создайте массив.\n\n");
+            }
+            menu();
+            break;
+        }
+        case 10:
+            printf("выход\n");
+            break;
+        default:
+            printf("Неверный выбор. Попробуйте снова.\n");
+            menu();
         }
     }
+
     if (globalArray != NULL) {
         free(globalArray);
     }
     if (originalArray != NULL) {
         free(originalArray);
     }
+
+    return 0;
 }
