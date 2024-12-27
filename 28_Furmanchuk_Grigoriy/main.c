b2212d4047bc64604874adad705ce2c608f56954
GrigoriyFurmanchuk10 <drippoststatus2gmail.com>
2024-12-27 13:34:32 +0300

    kassi end

--- 28_Furmanchuk_Grigoriy/main.c
+++ 28_Furmanchuk_Grigoriy/main.c
@@ -4,7 +4,7 @@
 #include "locale.h"
 #include "stdbool.h"
 #include <time.h>
-#define _CRT_SECURE_NO_WARNINGS
+//#define _CRT_SECURE_NO_WARNINGS
 
 
 struct prod {
@@ -12,12 +12,21 @@ struct prod {
 	int numof;
 };
 
+struct tovar {
+	int code;
+	char name[50];
+	double price;
+	double wt;
+};
+
+
 void get_list(struct prod arr[], int n) {
 	int i;
 	printf("Введите сначала название товара, а затем его количество количество в шт. \n ");
 	for (i = 0; i < n; i++) {
-		char s = arr[i].nameof;
-		fgets(s, sizeof(s), stdin);
+		char s[50];
+		//fgets(s, 50, stdin);
+		scanf_s("%s", arr[i].nameof, 50);
 		scanf_s("%d", &arr[i].numof);
 	}
 }
@@ -26,7 +35,7 @@ void get_list(struct prod arr[], int n) {
 //	return skl[id].wt - m;
 //}
 
-int sklad_check(char s[], struct tovar skl[]) {
+int sklad_check(char s[], struct tovar* skl) {
 	int i;
 	for (i = 0; i < 5;i++) {
 		if (strcmp(s, skl[i].name) == 0) return i;
@@ -47,29 +56,26 @@ void get_bill(struct prod arr[], int n, struct tovar skl[], struct tovar bll[]) 
 		bll[i].wt = 0.0;
 		bll[i].code = -1;*/
 		id = sklad_check(arr[i].nameof, skl);
-		strcpy(&bll[id].name, &skl[id].name);
-		kg = (double)rand() / (double)(RAND_MAX)* a;
-		mass = kg * arr[i].numof;
-		if (mass > skl[id].wt) {
-			printf("На складе находиться меньшее количество чем вам нужно. Забирайте весь склад\n");
-			mass = skl[id].wt;
+		if (id != -1) {
+			strcpy(&bll[id].name, &skl[id].name);
+			kg = (double)rand() / (double)(RAND_MAX)*a;
+			mass = kg * arr[i].numof;
+			if (mass > skl[id].wt) {
+				printf("На складе находиться меньшее количество чем вам нужно. Забирайте весь склад\n");
+				mass = skl[id].wt;
+			}
+			bll[id].price += mass * skl[id].price;
+			bll[id].wt += mass;
+			//bll[id].code = skl[id].code;
+			/*if (id != -1) {
+				printf("%s....%lf * %lf....%lf\n", skl[id].name, mass, skl[id].price, res);
+			}*/
 		}
-		bll[id].price += mass * skl[id].price;
-		bll[id].wt += mass;
-		//bll[id].code = skl[id].code;
-		/*if (id != -1) {
-			printf("%s....%lf * %lf....%lf\n", skl[id].name, mass, skl[id].price, res);
-		}*/
+		if (id == -1) printf("товара %s нет на складе\n", arr[i].nameof);
 		i += 1;
 	}
 }
 
-struct tovar {
-	int code;
-	char name[50];
-	double price;
-	double wt;
-};
 
 int main(void) {
 	int i, j;
@@ -77,17 +83,18 @@ int main(void) {
 	double sum = 0.0;
 	struct prod* list;
 	struct tovar bill[5] = { {0, "-", 0.0, 0.0}, {1, "-", 0.0, 0.0}, {2, "-", 0.0, 0.0}, {3, "-", 0.0, 0.0}, {4, "-", 0.0, 0.0} };
-	struct tovar sklad[5] = { {0, "банан", 100.0, 10.0}, {1, "яблоко", 101.0, 11.0}, {2, "томато", 105.0, 15.0}, {3, "кукумбер", 102.0, 12.0}, {4, "потато", 104.0, 14.0} };
+	struct tovar sklad[5] = { {0, "b", 100.0, 28.0}, {1, "a", 101.0, 32.3}, {2, "t", 105.0, 4.0}, {3, "o", 102.0, 19.0}, {4, "p", 104.0, 14.0} };
 	setlocale(LC_ALL, "rus");
 	printf("Количество товаров: ");
 	scanf_s("%d", &n);
 	list = (struct prod*)malloc(sizeof(struct prod) * n);
-	printf("Список: \n");
+	printf("Список товаров: \n");
 	get_list(list, n);
 	get_bill(list, n, sklad, bill);
 	for (i = 0; i < 5; i++) {
 		if (bill[i].price != 0.0) {
-			printf("%s....%lf * %lf....%lf\n", bill[i].name, bill[i].wt, sklad[i].price, bill[i].price);
+			
+			printf("%s....%lf * %lf....%lf\n",bill[i].name, bill[i].wt, bill[i].price / bill[i].wt, bill[i].price);
 			sum += bill[i].price;
 		}
 	}
