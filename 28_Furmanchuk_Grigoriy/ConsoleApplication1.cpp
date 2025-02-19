c649b5c9a787c42684fff448594f4d5b8d2d0cfe
unknown <T2081@Q113.local>
2025-02-19 16:01:41 +0300

    ya dawn

---
+++ 28_Furmanchuk_Grigoriy/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include "iostream"
+using namespace std;
+
+
+class dist {
+private:
+	double metr;
+public:
+	dist(double m = 0.0);
+	void setcurr(dist m2) {}
+	double getd() { return metr; }
+	double getmile() { return metr / 1609.34; }
+	double getyard() { return metr * 1.09361; }
+	double getfood() { return metr * 3.28084; }
+};
+
+
+
+	dist::dist(double m) {
+
+	}
+
+
+	void main() {
+
+	}
