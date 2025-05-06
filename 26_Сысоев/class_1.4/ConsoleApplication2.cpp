e86dd77c73fb36cbec91ca634877534fee682f8c
fleyao <80472074+vok1001@users.noreply.github.com>
2025-05-06 22:05:43 +0300

    Add files via upload
    добавлены решения задач за 26.02.25 и 12.03.25

---
+++ 26_Сысоев/class_1.4/ConsoleApplication2.cpp
@@ -0,0 +1,113 @@
+﻿// простой класс / 26.02.25 / 1.4. Конвертер длин /
+
+#include <iostream>
+#include <unordered_map>
+#include <iomanip>
+#include <locale>
+
+using namespace std;
+
+class Conv {
+private:
+    double m;
+    unordered_map<string, pair<string, double>> kf = {
+        {"ft", {"футы", 3.28}},
+        {"yd", {"ярды", 1.09}},
+        {"arsh", {"аршины", 1.41}},
+        {"sazh", {"сажени", 0.4686}},
+    };
+
+public:
+    Conv() : m(0) {}
+    Conv(double x) : m(x) {}
+
+    void set(double x) {
+        m = x;
+    }
+
+    void add(double x) {
+        m += x;
+    }
+
+    void sub(double x) {
+        m -= x;
+        if (m < 0) m = 0;
+    }
+
+    double get() const {
+        return m;
+    }
+
+    double conv(const string& u) const {
+        if (kf.find(u) != kf.end()) {
+            return m * kf.at(u).second;
+        }
+        else {
+            cerr << "Ошибка, неизвестная единица измерения!" << endl;
+            return 0;
+        }
+    }
+
+    void out(const string& unit = "") const {
+        cout.imbue(locale("")); 
+        cout << "Длина: " << m << " метров" << endl;
+
+        if (unit.empty()) {
+            for (const auto& p : kf) {
+                cout << " - " << p.second.first << ": " << fixed << setprecision(2) << conv(p.first) << endl;
+            }
+        }
+        else {
+            if (kf.find(unit) != kf.end()) {
+                cout << " - " << kf.at(unit).first << ": " << fixed << setprecision(2) << conv(unit) << endl;
+            }
+            else {
+                cout << "Ошибка: неизвестная единица измерения!" << endl;
+            }
+        }
+    }
+};
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+    Conv l;
+    double val;
+    int choice;
+    string unit;
+
+    while (true) {
+        cout << "\n1. установить длину (м)\n2. увеличить длину (м)\n3. уменьшить длину (м)\n4. показать длину (всех единицах)\n5. Показать длину (в выбранной единице)\n6. выйти\nВыберите действие: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            cout << "Введите длину (м): ";
+            cin >> val;
+            l.set(val);
+            break;
+        case 2:
+            cout << "На сколько увеличить (м): ";
+            cin >> val;
+            l.add(val);
+            break;
+        case 3:
+            cout << "На сколько уменьшить (м): ";
+            cin >> val;
+            l.sub(val);
+            break;
+        case 4:
+            l.out();
+            break;
+        case 5:
+            cout << "Введите код единицы измерения (ft, yd, arsh, sazh): ";
+            cin >> unit;
+            l.out(unit);
+            break;
+        case 6:
+            return 0;
+        default:
+            cout << "неизвестная команда!" << endl;
+        }
+    }
+}
---
+++ 26_Сысоев/class_2.3/ConsoleApplication3.cpp
@@ -0,0 +1,234 @@
+﻿// 2.3 Вектор,  12.03.25
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Vector {
+private:
+    int size;
+    int* data;
+
+public:
+
+    Vector() {
+        size = 0;
+        data = nullptr;
+    }
+
+    Vector(int s) {
+        size = s;
+        data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = 0;
+        }
+    }
+
+    Vector(const Vector& v) {
+        size = v.size;
+        data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = v.data[i];
+        }
+    }
+
+    Vector& operator=(const Vector& v) {
+        if (this == &v) return *this;
+
+        delete[] data;
+
+        size = v.size;
+        data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = v.data[i];
+        }
+        return *this;
+    }
+
+    ~Vector() {
+        delete[] data;
+    }
+
+    void setSize(int nSize) {
+        if (nSize == size) return;
+
+        delete[] data;
+
+        size = nSize;
+        data = new int[size];
+        for (int i = 0; i < size; i++) {
+            data[i] = 0;
+        }
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    void setComponent(int ind, int value) {
+        if (ind >= 0 && ind < size) {
+            data[ind] = value;
+        }
+        else {
+            cout << "Ошибка: выход за границы вектора" << endl;
+        }
+    }
+
+    int getComponent(int ind) {
+        if (ind >= 0 && ind < size) {
+            return data[ind];
+        }
+        else {
+            cout << "Ошибка: выход за границы вектора" << endl;
+            return 0;
+        }
+    }
+
+    double len() {
+        double sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += pow(data[i], 2);
+        }
+        return sqrt(sum);
+    }
+
+    // Перегрузка + 
+    Vector operator+(Vector& v) {
+        Vector result(size);
+        if (size != v.size) {
+            cout << "Ошибка, разный размер векторов или отсутствие одного из них" << endl;
+            return result;
+        }
+        for (int i = 0; i < size; i++) {
+            result.data[i] = data[i] + v.data[i];
+        }
+        return result;
+    }
+
+    // Перегрузка * 
+    int operator*(Vector& v) {
+        if (size != v.size) {
+            cout << "Ошибка, разный размер векторов или отсутствие одного из них" << endl;
+            return 0;
+        }
+        int res = 0;
+        for (int i = 0; i < size; i++) {
+            res += data[i] * v.data[i];
+        }
+        return res;
+    }
+
+    friend ostream& operator<<(ostream& os, Vector& v);
+};
+
+
+ostream& operator<<(ostream& ost, Vector& v) {
+    ost << "[ ";
+    for (int j = 0; j < v.size; j++) {
+        ost << v.data[j] << " ";
+    }
+    ost << "]";
+
+    return ost;
+}
+
+
+void fillVector(Vector& v) {
+    int n = v.getSize();
+    cout << "Введите " << n << " целых чисел:" << endl;
+    for (int i = 0; i < n; i++) {
+        int val;
+        cout << "Элемент [" << i << "]: ";
+        cin >> val;
+        v.setComponent(i, val);
+    }
+}
+
+
+void showMenu() {
+
+    cout << "\n==============================\n";
+    cout << "              МЕНЮ\n";
+    cout << "==============================\n";
+    cout << "1. Создать вектор\n";
+    cout << "2. Заполнить вектор \n";
+    cout << "3. Показать текущий вектор\n";
+    cout << "4. Длина вектора\n";
+    cout << "5. Скалярное произведение двух векторов\n";
+    cout << "6. Сложение двух векторов\n";
+    cout << "7. Выход\n";
+    cout << "==============================\n";
+    cout << "Выберите действие: ";
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    Vector v1, v2;
+    bool exit = false;
+
+    while (!exit) {
+        showMenu();
+        int choice;
+        cin >> choice;
+
+        switch (choice) {
+        case 1: {
+            int size;
+            cout << "Введите размер вектора: ";
+            cin >> size;
+            v1.setSize(size);
+            cout << "Вектор создан." << endl;
+            break;
+        }
+        case 2: {
+            if (v1.getSize() == 0) {
+                cout << "Для этого создайте сначала вектор" << endl;
+                break;
+            }
+            fillVector(v1);
+            break;
+        }
+        case 3: {
+            cout << "Текущий вектор: " << v1 << endl;
+            break;
+        }
+        case 4: {
+            cout << "Длина вектора: " << v1.len() << endl;
+            break;
+        }
+        case 5: {
+            cout << "Создайте второй вектор" << endl;
+            int size;
+            cout << "Размер: ";
+            cin >> size;
+            v2.setSize(size);
+            fillVector(v2);
+
+            cout << "Скалярное произведение: " << v1 * v2 << endl;
+            break;
+        }
+        case 6: {
+            cout << "Создайте второй вектор" << endl;
+            int size;
+            cout << "Размер: ";
+            cin >> size;
+            v2.setSize(size);
+            fillVector(v2);
+
+            Vector sum = v1 + v2;
+            cout << "Сумма векторов: " << sum << endl;
+            break;
+        }
+
+        case 7: {
+            exit = true;
+            cout << "Выход..." << endl;
+            break;
+        }
+        default:
+            cout << "Неверный выбор" << endl;
+        }
+    }
+
+    return 0;
+}
